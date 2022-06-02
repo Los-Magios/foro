6e0239d2f52c20db7d2e901a177b2467ad0a5a07
MarcoBG <marcoantprog@gmail.com>
2022-06-02 10:12:14 -0300

    class

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom"
 const Login = () => {
   return (
     <>
-    <div class="bg"></div>
-    <div class="bg bg2"></div>
-    <div class="bg bg3"></div>
-    <div class="content">
+    <div className="bg"></div>
+    <div className="bg bg2"></div>
+    <div className="bg bg3"></div>
+    <div className="content">
       <div className="container">
       <form action="" className='form'>
         <h1>Inicio de sesión</h1>
