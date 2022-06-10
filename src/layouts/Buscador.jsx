e4b1b2f631c3cd83ecf4b8a9a93ac48389d96ea4
G4brielG <gabriel240901@gmail.com>
2022-06-10 16:44:26 -0300

    si

---
+++ src/layouts/Buscador.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Buscador = () => {
+  return (
+    <>
+      <div class="input-group mb-3">
+        <input type="search" placeholder="Buscar" className="form-control" />
+        <button className="btn btn-primary"><i className="fas fa-search"></i></button>
+      </div>
+    </>
+  )
+}
+
+export default Buscador
--- src/pages/Foro.jsx
+++ src/pages/Foro.jsx
@@ -1,12 +1,14 @@
 // import FormPosts from "../layouts/FormPosts";
 import { Link } from "react-router-dom";
 import Nav from "../layouts/Nav";
+import Buscador from "../layouts/Buscador"
 
 const Foro = () => {
   return (
     <>
       <Nav />
       <div className="container border-0 shadow h-100 my--3 px-5">
+        <Buscador />
         <div className="row my-3">
           <div className="col text-center">
             <h4 className="mt-3">PREGUNTAS</h4>
@@ -17,20 +19,21 @@ const Foro = () => {
         </div>
 
         <ul className="list-group list-group-flush list my--3">
+
           <li className="list-group-item px-0 border-bottom">
             <div className="row align-items-center ">
-              <div className="col-auto">
+              <div className="col-auto ms--2">
+                <Link to="/verpost" className="text-decoration-none text-reset">
+                  <h4 className="h6 mt-1">Problema Ejemplo 001</h4>
+                </Link>
+              </div>
+              <div className="col d-flex justify-content-end">
                 <div>
                   <i className="far fa-thumbs-up"></i>5
                 </div>
                 <div>
                   <i className="far fa-solid fa-comments"></i>2
                 </div>
-              </div>
-              <div className="col-auto ms--2">
-                <Link to="/verpost" className="text-decoration-none text-reset">
-                  <h4 className="h6 mt-1">Problema Ejemplo 001</h4>
-                </Link>
               </div>
             </div>
           </li>
